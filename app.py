8423536a96a527cfdeb2216389fd221ce547cffd
vinoth <vinoth.suku@gmail.com>
2020-08-16 19:52:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
-import seaborn as sns
-import re
-from pandas.api.types import is_numeric_dtype, is_string_dtype, is_datetime64_any_dtype, is_bool_dtype, is_float_dtype
 
 st.write("Hello")
 st.markdown("Welcome")
