fc81bf88b4e505d5f956ecb7b44c001edbcda625
vinoth <vinoth.suku@gmail.com>
2020-08-16 18:40:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import seaborn as sns
+import re
+from pandas.api.types import is_numeric_dtype, is_string_dtype, is_datetime64_any_dtype, is_bool_dtype, is_float_dtype
+
+def main():
+	st.write("Hello")
+
+if __name__ == "__main__":
+    main()
